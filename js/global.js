34ee0caab6c21b64f1feda93c600ebf6c3688152
juliocesar94ls21 <juliocesar94ls21@gmail.com>
2025-09-10 15:48:38 -0300

    atualização de projeto 10/09

--- js/global.js
+++ js/global.js
@@ -2,57 +2,7 @@ $(document).ready(function(){
 	$( ".paragraf-txt" ).effect( "slide", {queue: false, duration: 500, complete: function(){  $( ".btn-transparent-eg" ).effect( "slide","fast") }} ); 
 	$(".col-img-wpeg").fadeIn({queue: false, duration: 1000}); 
 	
-	var elem1 = $(".whapper-title-icons"),
-		elem2 = $(".content-section-mt"),
-		elem3 = $(".content-section-cc"),
-		elem4 = $(".row-border-cf"),
-		elem5 = $(".content-ai"),
-		elem6 = $(".content-aa"),
-		elem7 = $(".content-mc");
-		elem8 = $(".col-form-esg"),
-		classElem = $(".ccactive2"),
-		elem9 = $(".row-tumbs1");
-		elem10 = $(".row-tumbs2");
-		elem11 = $(".row-tumbs3");
 	
-	animateElem(elem1, elem1.children(".icon-text").eq(0), "animated fadeInLeftBig");
-	animateElem(elem1, elem1.children(".icon-text").eq(1), "animated fadeInUpBig");
-	animateElem(elem1, elem1.children(".icon-text").eq(2), "animated fadeInRightBig");
-	animateElem(elem2, elem2.find(".title-section-mt"), "animated fadeInLeftBig");
-	animateElem(elem2, elem2.find(".text-section-mt"), "animated fadeInRightBig");
-	animateElem(elem2, elem2.find(".btn-primari-mt"), "animated bounceIn");
-	animateElem(elem3, elem3.find(".title-section-cc"), "animated rubberBand");
-	animateElem(elem3, elem3.find(".text-section-cc"), "animated fadeInLeftBig");
-	animateElem(elem3, elem3.find(".content-itens-cc"), "animated fadeInDown");
-	animateElem(elem4, elem4.find(".title-section-cf"), "animated fadeInLeftBig");
-	animateElem(elem4, elem4.find(".text-sec-cf"), "animated fadeInRightBig");
-	animateElem(elem4, elem4.find(".btn-cf"), "animated rubberBand");
-	animateElem(elem4, elem4.find(".col-img-cf"), "animated fadeInUpBig");
-	animateElem(elem5, elem5.find(".title-section-ai"), "animated fadeInLeftBig");
-	animateElem(elem5, elem5.find(".row-itens-ai"), "animated fadeInUpBig");
-	animateElem(elem6, elem6.find(".title-section-aa"), "animated fadeInRightBig");
-	animateElem(elem6, elem6.find(".row-itens-ai"), "animated fadeInUpBig");
-	animateElem(elem7, elem7.find(".title-section-mc"), "animated fadeInLeftBig");
-	animateElem(elem7, elem7.find(".row-itens-ai"), "animated fadeInUpBig");
-	animateElem(elem8, elem8.find(".row"), "animated fadeInLeftBig");
-	animateElem(elem9, elem9.children(".col-tumb-ano").eq(0), "animated fadeInLeftBig");
-	animateElem(elem9, elem9.children(".col-tumb-ano").eq(1), "animated fadeInUpBig");
-	animateElem(elem9, elem9.children(".col-tumb-ano").eq(2), "animated fadeInRightBig");
-	animateElem(elem10, elem10.children(".col-tumb-ano").eq(0), "animated fadeInUpBig");
-	animateElem(elem10, elem10.children(".col-tumb-ano").eq(1), "animated fadeInLeftBig");
-	animateElem(elem10, elem10.children(".col-tumb-ano").eq(2), "animated fadeInUpBig");
-	animateElem(elem10, elem10.children(".col-tumb-ano").eq(3), "animated fadeInRightBig");
-	animateElem(elem11, elem11.children(".col-tumb-ano").eq(0), "animated fadeInLeftBig");
-	animateElem(elem11, elem11.children(".col-tumb-ano").eq(1), "animated fadeInUpBig");
-	animateElem(elem11, elem11.children(".col-tumb-ano").eq(2), "animated fadeInRightBig");
-	
-	function animateElem(elemAtachment, elemAnimate, animation){
-		$(window).on("scroll", function(){
-			if($(this).scrollTop() >= elemAtachment.offset().top - elemAtachment.outerHeight() - 200){
-				elemAnimate.addClass(animation);
-			}
-		});
-	}
 	
 	$(".icon-right-cc, .icon-left-cc").on("click", function(){
 		if($(this).hasClass("active1")){
@@ -109,6 +59,13 @@ $(document).ready(function(){
 			$(".content-produto").show().addClass("page-active");	
 		}
 	});
+
+	$(".btn-praia-leste").on("click", function(){
+		$(".page-active").hide().removeClass("page-active");
+		$(".obra-praia").show().addClass("page-active");
+		 window.scrollTo(0, 0);
+	})
+
 	$(".btn-cf").on("click", function(){
 		$(".page-active").hide().removeClass("page-active");
 		$(".content-obra").show().addClass("page-active");
